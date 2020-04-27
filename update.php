d74eac784b481f7a6f7ffdc1ea63bcc161f65197
Santiago Rosa <santiagorrosa@gmail.com>
2020-04-26 22:31:15 -0300

    commit 26-04-20

--- update.php
+++ update.php
@@ -2,45 +2,72 @@
 session_start();
 include "./Modelo/Conexion.php";
 
-$json = json_decode(file_get_contents('php://input'));
-$updateList = $json->{'update'};
-$insertList = $json->{'insert'};
-$batchUpdateQuery = "";
-$batchInsertQuery = "";
+$data = json_decode(file_get_contents('php://input', true));
+insert($data->{'insert'});
+update($data->{'update'});
+delete($data->{'delete'});
+setDate($data->{'novelties'});
 
-foreach ($updateList as $item) {
-  $marca = $item->{'marca'};
-  $descripcion = $item->{'descripcion'};
-  $rubro = $item->{'rubro'};
-  $precio = $item->{'precio'};
-  $info = $item->{'info'};
-  $codigo = $item->{'codigo'};
-  $imagen = strtolower($codigo);
-  $batchUpdateQuery = $batchUpdateQuery . "update productos set marca = '$marca', rubro = '$rubro'," .
-    " aplicacion = '$descripcion', precio_lista = $precio, fecha_modif = now(), imagen = '$imagen', info = '$info' where codigo = '$codigo'; ";
+function insert($list)
+{
+  $query = "INSERT INTO productos(codigo, marca, rubro, aplicacion, precio_lista, precio_oferta, imagen, fecha_agregado, info) values ";
+  foreach ($list as $it) {
+    $codigo = $it->{'codigo'};
+    $descripcion = $it->{'descripcion'};
+    $marca = $it->{'marca'};
+    $rubro = $it->{'rubro'};
+    $precio = $it->{'precio'};
+    $precio_oferta = $it->{'precio_oferta'};
+    $info = $it->{'info'};
+    $imagen = $it->{'imagen'};
+    $query = $query . "('$codigo','$marca','$rubro','$descripcion',$precio,$precio_oferta,'$imagen', now(), '$info'),";
+    $query = substr($query, 0, -1);
+  }
+  $conexion = Conexion::conectar();
+  $conexion->query($query);
+  $conexion->close();
 }
 
-foreach ($insertList as $item) {
-  $batchInsertQuery = "insert into productos(codigo, marca, rubro, aplicacion, precio_lista, imagen, fecha_agregado, info) values ";
-  $marca = $item->{'marca'};
-  $descripcion = $item->{'descripcion'};
-  $rubro = $item->{'rubro'};
-  $precio = $item->{'precio'};
-  $info = $item->{'info'};
-  $codigo = $item->{'codigo'};
-  $imagen = strtolower($codigo);
-  $batchInsertQuery = $batchInsertQuery . "('$codigo','$marca','$rubro','$descripcion',$precio,'$imagen', now(), '$info'),";
+function update($list)
+{
+  $query = "";
+  foreach ($list as $it) {
+    $codigo = $it->{'codigo'};
+    $descripcion = $it->{'descripcion'};
+    $marca = $it->{'marca'};
+    $rubro = $it->{'rubro'};
+    $precio = $it->{'precio'};
+    $precio_oferta = $it->{'precio_oferta'};
+    $info = $it->{'info'};
+    $imagen = $it->{'imagen'};
+    $query = $query . "UPDATE productos set marca = '$marca', rubro = '$rubro',
+                    aplicacion = '$descripcion', precio_lista = $precio, precio_oferta = $precio_oferta,
+                     fecha_modif = now(), imagen = '$imagen', info = '$info' WHERE codigo = '$codigo'; ";
+  }
+  $conexion = Conexion::conectar();
+  $conexion->multi_query($query);
+  $conexion->close();
 }
-$batchInsertQuery = substr($batchInsertQuery, 0, -1); //Quito la ultima coma
 
-$conexion = Conexion::conectar();
-$conexion->multi_query($batchUpdateQuery);
-$conexion->close();
-$conexion = Conexion::conectar();
-$conexion->query($batchInsertQuery);
-$conexion->close();
+function delete($list)
+{
+  $query = "";
+  foreach ($list as $it) {
+    $codigo = $it->{'codigo'};
+    $query = $query . "DELETE FROM productos WHERE codigo = '$codigo';";
+  }
+  $conexion = Conexion::conectar();
+  $conexion->multi_query($query);
+  $conexion->close();
+}
 
-$jsonString = json_encode($json);
-$json->{'insert_query'} = json_encode($batchInsertQuery);
-$json->{'update_query'} = json_encode($batchUpdateQuery);
-echo json_encode($json);
+function setDate($list)
+{
+  $string = "";
+  foreach ($list as $it){
+    $string .= $it . ", ";
+  }
+  $conexion = Conexion::conectar();
+  $conexion->query("INSERT into act_lista(fecha, cambios) values(now(),'$string')");
+  $conexion->close();
+}
--- update_all.php
+++ update_all.php
@@ -7,28 +7,38 @@ $jsonList = json_decode(file_get_contents('php://input'));
 $batchUpdateQuery = "";
 $batchInsertQuery = "";
 
+$bdList = listaBd();
+
 foreach ($jsonList as $item) {
-  $marca = $item->{'marca'};
-  $descripcion = $item->{'descripcion'};
-  $rubro = $item->{'rubro'};
-  $precio = $item->{'precio'};
-  $info = $item->{'info'};
   $codigo = $item->{'codigo'};
-  $imagen = strtolower($codigo);
-  $batchUpdateQuery = $batchUpdateQuery . "update productos set marca = '$marca', rubro = '$rubro'," .
-    " aplicacion = '$descripcion', precio_lista = $precio, fecha_modif = now(), imagen = '$imagen', info = '$info' where codigo = '$codigo'; ";
+  if ($bdList[$codigo] != null && $bdList['precio_lista'] != $item->{'precio'}) {
+    $marca = $item->{'marca'};
+    $descripcion = $item->{'descripcion'};
+    $rubro = $item->{'rubro'};
+    $precio = $item->{'precio'};
+    $precio_oferta = $item->{'precio_oferta'};
+    $info = $item->{'info'};
+    $codigo = $item->{'codigo'};
+    $imagen = strtolower($codigo);
+    $batchUpdateQuery = $batchUpdateQuery . "update productos set marca = '$marca', rubro = '$rubro'," .
+      " aplicacion = '$descripcion', precio_lista = $precio, precio_oferta = $precio_oferta, fecha_modif = now(), imagen = '$imagen', info = '$info' where codigo = '$codigo'; ";
+  }
 }
 
 foreach ($jsonList as $item) {
-  $batchInsertQuery = "insert into productos(codigo, marca, rubro, aplicacion, precio_lista, imagen, fecha_agregado, info) values ";
-  $marca = $item->{'marca'};
-  $descripcion = $item->{'descripcion'};
-  $rubro = $item->{'rubro'};
-  $precio = $item->{'precio'};
-  $info = $item->{'info'};
   $codigo = $item->{'codigo'};
-  $imagen = strtolower($codigo);
-  $batchInsertQuery = $batchInsertQuery . "('$codigo','$marca','$rubro','$descripcion',$precio,'$imagen', now(), '$info'),";
+  if ($bdList[$codigo] == null) {
+    $batchInsertQuery = "insert into productos(codigo, marca, rubro, aplicacion, precio_lista, precio_oferta, imagen, fecha_agregado, info) values ";
+    $marca = $item->{'marca'};
+    $descripcion = $item->{'descripcion'};
+    $rubro = $item->{'rubro'};
+    $precio = $item->{'precio'};
+    $precio_oferta = $item->{'precio_oferta'};
+    $info = $item->{'info'};
+    $codigo = $item->{'codigo'};
+    $imagen = strtolower($codigo);
+    $batchInsertQuery = $batchInsertQuery . "('$codigo','$marca','$rubro','$descripcion',$precio,$precio_oferta,'$imagen', now(), '$info'),";
+  }
 }
 $batchInsertQuery = substr($batchInsertQuery, 0, -1); //Quito la ultima coma
 
@@ -39,3 +49,19 @@ echo json_encode($batchUpdateQuery);
 //$conexion = Conexion::conectar();
 //$conexion->query($batchInsertQuery);
 //$conexion->close();
+
+
+function listaBd()
+{
+  $conexion = Conexion::conectar();
+  $itemMap = [];
+  $query = "select codigo, marca, rubro, aplicacion, precio_lista, imagen from productos";
+  $resultset = $conexion->query($query);
+  while ($row = $resultset->fetch_assoc()) {
+    $row['vigente'] = 0;
+    $itemMap[$row["codigo"]] = $row;
+  }
+  $resultset->free();
+  $conexion->close();
+  return $itemMap;
+}
